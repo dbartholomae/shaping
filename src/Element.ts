a8c07e0321f207e65f272de3a1f74047abb3412b
Daniel Bartholomae <daniel@bartholomae.name>
2024-12-13 16:08:05 +0100

    Add shFile element

--- src/Element.ts
+++ src/Element.ts
@@ -1,8 +1,12 @@
 import type { ElementConstructor } from "./ElementConstructor";
 
+export const ShFileType = "shFile" as const;
+
+export type Type = ElementConstructor<any> | typeof ShFileType;
+
 export interface Element<
   P = unknown,
-  T extends string | ElementConstructor<any> = string | ElementConstructor<any>,
+  T extends Type = Type,
 > {
   type: T;
   props: P;
--- src/JSX.ts
+++ src/JSX.ts
@@ -1,5 +1,15 @@
-declare namespace JSX {
-  interface ElementChildrenAttribute {
-    children: {}; // specify children name to use
+export {}
+
+declare global {
+  namespace JSX {
+    interface ElementChildrenAttribute {
+      children: {}; // specify children name to use
+    }
+    interface IntrinsicElements {
+      shFile: {
+        name: string;
+        content: string;
+      }
+    }
   }
 }
